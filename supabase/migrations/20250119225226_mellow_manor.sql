ce42488581d168b1550997ac90628b4f1bece8e6
FaiqaArshad <faiqaarshad563@gmail.com>
2025-02-11 22:18:36 +0500

    remove migartions

--- supabase/migrations/20250119225226_mellow_manor.sql
+++
@@ -1,8 +0,0 @@
--- Add dark_logo_url column to site_settings table
-ALTER TABLE site_settings
-ADD COLUMN IF NOT EXISTS dark_logo_url text;
-
--- Update existing site settings to have null dark_logo_url
-UPDATE site_settings
-SET dark_logo_url = NULL
-WHERE dark_logo_url IS NULL;
